b7e8fdd59bcad86dbbcd5c325cd4c694e3bc9e62
RodrigooGuedes <rodrigogueedes7@gmail.com>
2026-05-09 09:48:51 -0300

    Entidade projeto

---
+++ src/entidades/Projeto.java
@@ -0,0 +1,89 @@
+package entidades;
+
+import java.time.LocalDate;
+
+import enums.StatusProjeto;
+
+public class Projeto {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate datConclusao;
+    private StatusProjeto status;
+    private Usuario responsavel;
+
+    public Projeto() {
+    }
+
+    public Projeto(Long id, String nome, String descricao, LocalDate dataInicio, LocalDate datConclusao,
+            StatusProjeto status, Usuario responsavel) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataInicio = dataInicio;
+        this.datConclusao = datConclusao;
+        this.status = status;
+        this.responsavel = responsavel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDatConclusao() {
+        return datConclusao;
+    }
+
+    public void setDatConclusao(LocalDate datConclusao) {
+        this.datConclusao = datConclusao;
+    }
+
+    public StatusProjeto getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusProjeto status) {
+        this.status = status;
+    }
+
+    public Usuario getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(Usuario responsavel) {
+        this.responsavel = responsavel;
+    }
+    
+    
+    
+
+}
